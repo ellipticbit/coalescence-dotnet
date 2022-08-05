cf0ac03f3bf07ed3ad7c40fe9bccdd76209fb3d1
Adam Wilson <adamw@ellipticbit.com>
2022-08-05 00:41:02 -0700

    Missed save

---
+++ LexiconResponseError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+
+namespace EllipticBit.Lexicon.Client
+{
+	public sealed class LexiconResponseError : Exception
+	{
+		public HttpStatusCode StatusCode { get; }
+		public string Content { get; }
+
+		internal LexiconResponseError(HttpStatusCode statusCode, string message, string content) : base(message)
+		{
+			StatusCode = statusCode;
+			Content = content;
+		}
+
+		public override string ToString() {
+			return $"HTTP {StatusCode}: {Message}";
+		}
+	}
+}
