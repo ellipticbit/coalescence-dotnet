a5df751e5a102afd4b44b7793295f748431449a1
Adam Wilson <adamw@ellipticbit.com>
2023-08-18 15:33:44 -0700

    Add support for setting options on the default serializers.

--- Shared/HotwireOptionsBase.cs
+++ Shared/HotwireOptionsBase.cs
@@ -26,11 +26,14 @@ namespace EllipticBit.Hotwire.Shared
 		/// <summary>
 		/// Default constructor for Hotwire options classes.
 		/// </summary>
-		protected HotwireOptionsBase(string name = null) {
+		/// <param name="name">Name of the options class used with creating a request.</param>
+		/// <param name="jsonOptions">Serializer options for the default JSON serializer.</param>
+		/// <param name="xmlOptions">Serializer options for the default XML serializer.</param>
+		protected HotwireOptionsBase(string name, JsonSerializerOptions jsonOptions = null, XmlSerializationOptions xmlOptions = null) {
 			Name = name;
 			Serializers = new IHotwireSerializer[] {
-				new HotwireJsonSerializer(new JsonSerializerOptions(), true),
-				new HotwireXmlSerializer(new XmlSerializationOptions(), false)
+				new HotwireJsonSerializer(jsonOptions ?? new JsonSerializerOptions(), true),
+				new HotwireXmlSerializer(xmlOptions ?? new XmlSerializationOptions(), false)
 			};
 			Authenticators = new IHotwireAuthentication[] {
 				new HotwireNullAuthentication()
@@ -43,11 +46,14 @@ namespace EllipticBit.Hotwire.Shared
 		/// <param name="name">A name for this set options.</param>
 		/// <param name="serializers">Optional parameter to initialize a custom set of Serialization handlers.</param>
 		/// <param name="authenticators">Optional parameter to initialize a custom set of Authentication handlers.</param>
-		protected HotwireOptionsBase(string name, IEnumerable<IHotwireSerializer> serializers = null, IEnumerable<IHotwireAuthentication> authenticators = null)
+		protected HotwireOptionsBase(string name, IEnumerable<IHotwireSerializer> serializers, IEnumerable<IHotwireAuthentication> authenticators)
 		{
 			Name = name;
-			Serializers = serializers;
-			Authenticators = authenticators;
+			Serializers = serializers ?? new IHotwireSerializer[] {
+				new HotwireJsonSerializer(new JsonSerializerOptions(), true),
+				new HotwireXmlSerializer(new XmlSerializationOptions(), false)
+			};
+			Authenticators = authenticators ?? new IHotwireAuthentication[] { new HotwireNullAuthentication() };
 		}
 	}
 }
