7698f509cde7c1e11a054b1632f0f26d3aaa8fdc
Adam Wilson <adamw@ellipticbit.com>
2023-10-14 03:15:31 -0700

    Add BindingObject

---
+++ Shared/BindingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EllipticBit.Coalescence.Shared
+{
+	public abstract class BindingObject : INotifyPropertyChanged, INotifyPropertyChanging
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public event PropertyChangingEventHandler PropertyChanging;
+
+		protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+		{
+			if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+			PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+			field = value;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			return true;
+		}
+	}
+}
