cd49653ad45cec11b639f2009d85b92ef4d28093
Adam Wilson <adamw@ellipticbit.com>
2024-02-14 02:32:39 -0800

    Add extension methods for single-value Queries and Headers.

---
+++ Shared/Request/ICoalescenceRequestBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EllipticBit.Coalescence.Shared.Request
+{
+	public static class ICoalescenceRequestBuilderExtensions
+	{
+		public static ICoalescenceRequestBuilder Query(this ICoalescenceRequestBuilder builder, string key, string value) {
+			return builder.Query(key, new [] { value });
+		}
+
+		public static ICoalescenceRequestBuilder Query(this ICoalescenceRequestBuilder builder, string key, byte[] value) {
+			return builder.Query(key, new [] { value });
+		}
+
+		public static ICoalescenceRequestBuilder Query<T>(this ICoalescenceRequestBuilder builder, string key, T value) where T : unmanaged, IComparable, IFormattable {
+			return builder.Query(key, new[] { value });
+		}
+		public static ICoalescenceRequestBuilder Query<T>(this ICoalescenceRequestBuilder builder, string key, T? value) where T : unmanaged, IComparable, IFormattable {
+			return builder.Query(key, new[] { value });
+		}
+
+		public static ICoalescenceRequestBuilder Header(this ICoalescenceRequestBuilder builder, string key, string value) {
+			return builder.Header(key, new[] { value });
+		}
+
+		public static ICoalescenceRequestBuilder Header(this ICoalescenceRequestBuilder builder, string key, byte[] value) {
+			return builder.Header(key, new[] { value });
+		}
+
+		public static ICoalescenceRequestBuilder Header<T>(this ICoalescenceRequestBuilder builder, string key, T value) where T : unmanaged, IComparable, IFormattable {
+			return builder.Header(key, new[] { value });
+		}
+
+		public static ICoalescenceRequestBuilder Header<T>(this ICoalescenceRequestBuilder builder, string key, T? value) where T : unmanaged, IComparable, IFormattable {
+			return builder.Header(key, new[] { value });
+		}
+	}
+}
