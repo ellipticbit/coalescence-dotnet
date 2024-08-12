72466542580b5e9364b865548ff1946712453125
Adam Wilson <adamw@ellipticbit.com>
2024-08-12 02:44:53 -0700

    Add support for ZStandard compression options.

---
+++ Shared/ZStdCompressionOptions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace EllipticBit.Coalescence.Shared
+{
+	public enum ZStdCompressionLevel
+	{
+		None = 0,
+		Fastest = 1,
+		Fast = 3,
+		Balanced = 5,
+		Slow = 9,
+		Slower = 12,
+		Slowest = 19
+	}
+
+	public class ZStdCompressionOptions : IOptions<ZStdCompressionOptions>
+	{
+		public ZStdCompressionLevel Level { get; set; } = ZStdCompressionLevel.Balanced;
+
+		/// <inheritdoc />
+		ZStdCompressionOptions IOptions<ZStdCompressionOptions>.Value => this;
+	}
+}
